8fd8134e8fdf70106ded7ffac6a32315f2bb2f12
athuldas1 <athuldas834@gmail.com>
2025-10-01 15:41:31 +0530

    delete backend

--- public/donation/fddonationapp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from.models import user,user2,log,food,accepting
-
-# Register your models here.
-admin.site.register(user)
-admin.site.register(user2)
-admin.site.register(log)
-admin.site.register(food)
-admin.site.register(accepting)
